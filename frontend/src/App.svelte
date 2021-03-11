83e634b81a92c16e5e18ecae10e94d19b116f0c6
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2021-03-11 19:25:27 +0100

    Move Tailwind to seperate css file

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-	import TailwindCss from './TailwindCss.svelte';
-
 	export let name: string;
 </script>
 
@@ -8,5 +6,3 @@
 	<h1 class="text-3xl color text-green-700">{name}!</h1>
 	<p>Cybernetically improved taboo game</p>
 </main>
-
-<TailwindCss />
