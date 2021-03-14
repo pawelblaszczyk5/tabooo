52273018f5bce152bf9c6145c227a3fb195caa2f
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2021-03-14 18:28:34 +0100

    Fix shadow on buttons

--- frontend/src/components/commons/Button.svelte
+++ frontend/src/components/commons/Button.svelte
@@ -12,15 +12,17 @@
 	export let type: ButtonType = ButtonType.PRIMARY;
 </script>
 
-<div class="relative overflow-hidden rounded {classes}">
-	<button
-		type="button"
-		use:ripple
-		class="px-5 py-2 select-none rounded text-xl shadow-2xl focus:outline-none
-	{type === ButtonType.PRIMARY
-			? 'bg-primaryAccent hover:bg-primaryAccentHover focus:bg-primaryAccentFocus text-primaryFontColor'
-			: 'bg-secondaryAccent hover:bg-secondaryAccentHover focus:bg-secondaryAccentFocus text-secondaryFontColor'}"
-		on:click>
-		<slot />
-	</button>
+<div class="shadow-2xl {classes}">
+	<div class="overflow-hidden relative rounded">
+		<button
+			type="button"
+			use:ripple
+			class="px-5 py-2 select-none rounded text-xl focus:outline-none
+		{type === ButtonType.PRIMARY
+				? 'bg-primaryAccent hover:bg-primaryAccentHover focus:bg-primaryAccentFocus text-primaryFontColor'
+				: 'bg-secondaryAccent hover:bg-secondaryAccentHover focus:bg-secondaryAccentFocus text-secondaryFontColor'}"
+			on:click>
+			<slot />
+		</button>
+	</div>
 </div>
