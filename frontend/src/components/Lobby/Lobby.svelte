c1a0148891ad8ef8bd53083e7d5fb6e1344e137b
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2021-03-20 09:28:42 +0100

    Added user media audio access

--- frontend/src/components/Lobby/Lobby.svelte
+++ frontend/src/components/Lobby/Lobby.svelte
@@ -2,15 +2,25 @@
 	import type {Socket} from 'socket.io-client';
 	import {io} from 'socket.io-client';
 	import {onMount} from 'svelte';
+	import {push} from 'svelte-spa-router';
 
 	let socket: Socket;
 
-	onMount(() => {
+	const initializeSockets = () => {
 		socket = io('', {path: '/api/ws/'});
 
 		socket.on('connect', () => {
 			console.log(socket.id);
 		});
+	};
+
+	const handleError = () => {
+		window.alert('You need to enable microphone access to play this game');
+		push('/');
+	};
+
+	onMount(() => {
+		navigator.getUserMedia({audio: true}, initializeSockets, handleError);
 	});
 </script>
 
