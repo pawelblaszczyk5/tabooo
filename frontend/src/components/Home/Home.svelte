6a530d8a7e96db4afb159ced66be514de47abfc8
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2021-03-26 20:58:46 +0100

    Delete placeholder squares from home

--- frontend/src/components/Home/Home.svelte
+++ frontend/src/components/Home/Home.svelte
@@ -6,5 +6,3 @@
 <Intro />
 <Button classes="my-5" type={ButtonType.PRIMARY} />
 <Button type={ButtonType.SECONDARY} />
-<div class="w-52 h-52 bg-primaryFirstTeam transition-colors duration-300" />
-<div class="w-52 h-52 bg-primarySecondTeam transition-colors duration-300" />
