cee782d7a1a300d62407a9cb0ce6925259c93d75
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2021-03-26 21:04:22 +0100

    Add spacer component

--- frontend/src/components/Home/Home.svelte
+++ frontend/src/components/Home/Home.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
 	import Button, {ButtonType} from '../commons/Button.svelte';
+	import Spacer from '../commons/Spacer.svelte';
 	import Intro from './Intro.svelte';
 </script>
 
 <Intro />
-<Button type={ButtonType.PRIMARY} />
-<Button type={ButtonType.SECONDARY} />
+<Spacer y={3}>
+	<Button type={ButtonType.PRIMARY} />
+</Spacer>
+<Spacer y={3}>
+	<Button type={ButtonType.SECONDARY} />
+</Spacer>
---
+++ frontend/src/components/commons/Spacer.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	export let x = 0;
+	export let y = 0;
+</script>
+
+<div class="px-{x} py-{y}">
+	<slot />
+</div>
