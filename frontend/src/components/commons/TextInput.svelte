5b90271108905166b294b52a1be8e4466258d7b3
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2021-03-26 21:00:06 +0100

    Add TextInput component

---
+++ frontend/src/components/commons/TextInput.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	export let value: string;
+	export let placeholder = '';
+</script>
+
+<label>
+	<p><slot /></p>
+	<input
+		{placeholder}
+		class="px-2 py-1 bg-transparent border-b-2 border-primaryAccent text-primaryFontColor font-semibold outline-none placeholder-opacity-0 placeholder-primaryFontColor
+    focus:border-primaryAccentFocus"
+		bind:value
+		type="text" />
+</label>
