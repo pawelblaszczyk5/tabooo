7b2da386cf7f0f26aca5fb460422209ccd886032
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2021-03-29 20:56:00 +0200

    Add back to home button

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,27 +1,43 @@
 <script lang="ts">
-	import Router from 'svelte-spa-router';
+	import Router, {push} from 'svelte-spa-router';
+	import FaArrowLeft from 'svelte-icons/fa/FaArrowLeft.svelte';
+	import type {RouteLoadedEvent} from 'svelte-spa-router';
 	import Modal from './components/commons/Modal.svelte';
 	import Settings from './components/App/Settings.svelte';
 	import Theme from './components/App/Theme.svelte';
 	import {routes} from './routes';
 	import FaCog from 'svelte-icons/fa/FaCog.svelte';
 
+	const homeRoutes = ['*', '/'];
+
 	let showSettingsModal = false;
+	let showBackHome = false;
 
 	const toggleModalState = (state: boolean) => {
 		showSettingsModal = state;
 	};
+
+	const routeLoaded = (event: RouteLoadedEvent) => {
+		showBackHome = !homeRoutes.includes(event.detail.route + '');
+	};
 </script>
 
 <main class="flex flex-col items-center p-6 min-h-screen w-screen bg-primary transition-colors duration-300">
-	<Router {routes} />
-	<Theme />
+	<Router on:routeLoaded={routeLoaded} {routes} />
 	<div
 		on:click={() => toggleModalState(!showSettingsModal)}
 		class="absolute top-4 right-4 w-8 h-8 cursor-pointer transform hover:rotate-90 transition-transform duration-500">
 		<FaCog />
 	</div>
+	{#if showBackHome}
+		<div
+			on:click={() => push('/')}
+			class="absolute top-4 left-4 w-8 h-8 cursor-pointer transform hover:scale-125 transition-transform duration-500">
+			<FaArrowLeft />
+		</div>
+	{/if}
 	{#if showSettingsModal}
 		<Modal on:closeModal={() => toggleModalState(false)}><Settings /></Modal>
 	{/if}
+	<Theme />
 </main>
