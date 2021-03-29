39d497912930ef2ca1265b5a7501e898e5246062
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2021-03-29 20:14:28 +0200

    Move NicknameModal to commons

---
+++ frontend/src/components/commons/NicknameModal.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import {createEventDispatcher} from 'svelte';
+	import Modal from './Modal.svelte';
+	import Spacer from './Spacer.svelte';
+	import TextInput from './TextInput.svelte';
+	import Button from './Button.svelte';
+
+	const dispatch = createEventDispatcher<{nicknameSet: string}>();
+
+	let nickname: string;
+
+	const changeNickname = () => {
+		dispatch('nicknameSet', nickname);
+	};
+</script>
+
+<Modal preventExit={true}>
+	<div class="flex flex-col items-center">
+		<p>Nickname is required to proceed</p>
+		<form class="flex flex-col items-center" on:submit|preventDefault={changeNickname}>
+			<Spacer y={5}>
+				<TextInput placeholder="Enter nickname..." bind:value={nickname} />
+			</Spacer>
+			<Button>Save</Button>
+		</form>
+	</div>
+</Modal>
