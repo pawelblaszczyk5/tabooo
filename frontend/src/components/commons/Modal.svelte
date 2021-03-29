c4b2628ec5f6ce463d5777887c8adf500d020b1f
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2021-03-29 20:07:10 +0200

    Move modal to document body

--- frontend/src/components/commons/Modal.svelte
+++ frontend/src/components/commons/Modal.svelte
@@ -2,6 +2,7 @@
 	import FaTimes from 'svelte-icons/fa/FaTimes.svelte';
 	import {createEventDispatcher} from 'svelte';
 	import {fly} from 'svelte/transition';
+	import Portal from 'svelte-portal/src/Portal.svelte';
 
 	export let preventExit = false;
 
@@ -12,12 +13,14 @@
 	};
 </script>
 
-<div class="absolute w-screen h-screen z-10 top-0 left-0 right-0 bottom-0" on:click={closeModal} />
-<div
-	transition:fly={{duration: 500, y: -200}}
-	class="z-20 glass-sm transition-colors duration-300 border border-gray-200 border-opacity-40 absolute top-48 left-1/2 transform -translate-x-1/2 p-5 sm:p-10 bg-primary rounded-2xl">
-	{#if !preventExit}
-		<div on:click={closeModal} class="absolute top-3 right-3 w-6 h-6 cursor-pointer"><FaTimes /></div>
-	{/if}
-	<slot />
-</div>
+<Portal target={document.body}>
+	<div class="absolute w-screen h-screen z-10 top-0 left-0 right-0 bottom-0" on:click={closeModal} />
+	<div
+		transition:fly={{duration: 500, y: -200}}
+		class="z-20 glass-sm transition-colors duration-300 border border-gray-200 border-opacity-40 absolute top-48 left-1/2 transform -translate-x-1/2 p-5 sm:p-10 bg-primary rounded-2xl">
+		{#if !preventExit}
+			<div on:click={closeModal} class="absolute top-3 right-3 w-6 h-6 cursor-pointer"><FaTimes /></div>
+		{/if}
+		<slot />
+	</div>
+</Portal>
