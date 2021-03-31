394e364d4352fbd6aa2eb33a82ec8352eedd8d04
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2021-03-31 20:47:33 +0200

    Add shadow to toastr

--- frontend/src/components/commons/Toastr.svelte
+++ frontend/src/components/commons/Toastr.svelte
@@ -3,7 +3,10 @@
 	import FaExclamationTriangle from 'svelte-icons/fa/FaExclamationTriangle.svelte';
 </script>
 
-<div class="w-64 p-3 bg-primaryAccent rounded flex items-center" in:fly={{x: -300, duration: 1000}} out:fly={{x: 300, duration: 1000}}>
+<div
+	class="w-64 p-3 bg-primaryAccent rounded flex items-center shadow-2xl"
+	in:fly={{x: -300, duration: 1000}}
+	out:fly={{x: 300, duration: 1000}}>
 	<div class="w-5 h-5 mr-2 flex-shrink-0"><FaExclamationTriangle /></div>
 	<p class="text-base font-semibold"><slot /></p>
 </div>
