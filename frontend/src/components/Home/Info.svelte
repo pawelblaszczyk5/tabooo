380f154e170b092908de451465516b3dfbbf7d6d
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2021-04-05 15:08:34 +0200

    Punctuation fix v2

--- frontend/src/components/Home/Info.svelte
+++ frontend/src/components/Home/Info.svelte
@@ -5,8 +5,8 @@
 
 <div class="flex flex-wrap items-center justify-center max-w-full sm:max-w-md w-96 sm:w-max p-2 sm:p-4">
 	<p class="text-center w-full mb-3">
-		Tabooo is a cybernetically enhanced taboo game built with Svelte, as catchphrase can suggest. The goal is simple–describe a word to your
-		team without using given words and let them guess it. Better team wins, good luck and have fun!
+		Tabooo is a cybernetically enhanced taboo game built with Svelte, as catchphrase can suggest. The goal is simple – describe a word to
+		your team without using given words and let them guess it. Better team wins, good luck and have fun!
 	</p>
 	<a
 		href="https://github.com/pawelblaszczyk5"
