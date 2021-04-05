e312cd921c6257cbee43984f16b9709707371fc5
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2021-04-05 13:16:38 +0200

    Button styles adjustments, smaller font, fix max width

--- frontend/src/components/commons/Button.svelte
+++ frontend/src/components/commons/Button.svelte
@@ -11,11 +11,11 @@
 	export let type: ButtonType = ButtonType.PRIMARY;
 </script>
 
-<div class="shadow-2xl w-max">
+<div class="shadow-2xl w-max max-w-full">
 	<div class="overflow-hidden relative rounded">
 		<button
 			use:ripple
-			class="px-5 py-2 select-none rounded text-xl focus:outline-none transition-colors duration-300
+			class="px-5 py-2 select-none rounded text-lg focus:outline-none transition-colors duration-300
 				{type === ButtonType.PRIMARY
 				? 'bg-primaryAccent hover:bg-primaryAccentHover focus:bg-primaryAccentFocus text-primaryFontColor'
 				: 'bg-secondaryAccent hover:bg-secondaryAccentHover focus:bg-secondaryAccentFocus text-secondaryFontColor'}"
