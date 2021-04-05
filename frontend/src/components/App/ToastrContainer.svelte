2709ed38e6882f20e23be06b93745b177b6aafde
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2021-04-05 14:33:22 +0200

    Change few absolute positioned items to fixed

--- frontend/src/components/App/ToastrContainer.svelte
+++ frontend/src/components/App/ToastrContainer.svelte
@@ -3,7 +3,7 @@
 	import Toastr from '../commons/Toastr.svelte';
 </script>
 
-<div class="absolute bottom-4 right-4">
+<div class="fixed bottom-4 right-4">
 	{#each $toastr as singleToastr}
 		<Toastr>{singleToastr.message}</Toastr>
 	{/each}
