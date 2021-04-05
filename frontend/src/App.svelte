7f40ca0a91fa9fa6f710f93ecf3133173c23a915
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2021-04-05 15:34:41 +0200

    Bring back min height of app component

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -30,7 +30,7 @@
 	};
 </script>
 
-<main class="flex flex-col mt-8 sm:mt-2 items-center p-6 w-screen bg-primary transition-colors duration-300">
+<main class="flex flex-col mt-8 sm:mt-2 items-center p-6 min-h-screen w-screen bg-primary transition-colors duration-300">
 	<Router on:routeLoaded={routeLoaded} {routes} />
 	<div
 		on:click={() => toggleModalState(!showSettingsModal)}
