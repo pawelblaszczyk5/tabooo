56902481d09bd13ea59700adb844386437721f68
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2021-04-05 14:30:56 +0200

    Add info modal

---
+++ frontend/src/components/Home/Info.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import FaGithub from 'svelte-icons/fa/FaGithub.svelte';
+	import FaLinkedin from 'svelte-icons/fa/FaLinkedin.svelte';
+</script>
+
+<div class="flex flex-wrap items-center justify-center max-w-full sm:max-w-xs w-96 sm:w-max p-2 sm:p-4">
+	<p class="text-center w-full mb-3">
+		Tabooo is a cybernetically enhanced taboo game built with Svelte, as catchphrase can suggest. The goal is simple - describe a word to
+		your team without using given words and let them guess it. Better team wins, good luck and have fun!
+	</p>
+	<a
+		href="https://github.com/pawelblaszczyk5"
+		class="block w-8 h-8 transform hover:scale-125 transition-transform duration-500 mx-2"
+		target="_blank">
+		<FaGithub />
+	</a>
+	<a
+		href="https://www.linkedin.com/in/pawel-blaszczyk/"
+		class="block w-8 h-8 transform hover:scale-125 transition-transform duration-500 mx-2"
+		target="_blank">
+		<FaLinkedin />
+	</a>
+</div>
