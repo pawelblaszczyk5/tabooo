15ea0bc1e1ae3b24c1f84f1956a46ee03eb6bdb8
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2021-04-08 20:05:33 +0200

    Added transitions between teams

--- frontend/src/components/Lobby/Players.svelte
+++ frontend/src/components/Lobby/Players.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import {get} from 'svelte/store';
+	import {quintOut} from 'svelte/easing';
+	import {crossfade} from 'svelte/transition';
 	import {Team} from '../../model/team';
 	import type {TeamChange} from '../../model/teamChange';
 	import {players} from '../../stores/players';
@@ -8,6 +10,23 @@
 	import Spacer from '../commons/Spacer.svelte';
 	import Player from './Player.svelte';
 	import RemoteAudio from './RemoteAudio.svelte';
+
+	const [send, receive] = crossfade({
+		duration: (d) => Math.sqrt(d * 200),
+		fallback(node: Element) {
+			const style = getComputedStyle(node);
+			const transform = style.transform === 'none' ? '' : style.transform;
+
+			return {
+				duration: 600,
+				easing: quintOut,
+				css: (t) => `
+					transform: ${transform} scale(${t});
+					opacity: ${t}
+				`,
+			};
+		},
+	});
 
 	const changeTeam = (team: Team) => {
 		const localSocket = get(socket);
@@ -25,23 +44,29 @@
 <div class="flex my-4">
 	<div class="mx-3 w-40">
 		{#each $players.filter((player) => player.team === Team.FIRST) as player (player.id)}
-			<Spacer y={2}>
-				<Player {player} />
-			</Spacer>
+			<div in:receive={{key: player.id}} out:send={{key: player.id}}>
+				<Spacer y={2}>
+					<Player {player} />
+				</Spacer>
+			</div>
 		{/each}
 	</div>
 	<div class="mx-3 w-40">
 		{#each $players.filter((player) => player.team === Team.OBSERVER) as player (player.id)}
-			<Spacer y={2}>
-				<Player {player} />
-			</Spacer>
+			<div in:receive={{key: player.id}} out:send={{key: player.id}}>
+				<Spacer y={2}>
+					<Player {player} />
+				</Spacer>
+			</div>
 		{/each}
 	</div>
 	<div class="mx-3 w-40">
 		{#each $players.filter((player) => player.team === Team.SECOND) as player (player.id)}
-			<Spacer y={2}>
-				<Player {player} />
-			</Spacer>
+			<div in:receive={{key: player.id}} out:send={{key: player.id}}>
+				<Spacer y={2}>
+					<Player {player} />
+				</Spacer>
+			</div>
 		{/each}
 	</div>
 </div>
