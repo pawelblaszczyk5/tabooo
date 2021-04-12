39e7eab0565c6f42522b13961c9312350e6c6329
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2021-04-12 19:52:05 +0200

    GroupPicker component added

---
+++ frontend/src/components/commons/GroupPicker.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	export let values: Array<string | number>;
+	export let group: string | number;
+</script>
+
+<div class="flex w-full rounded-md overflow-hidden">
+	{#each values as value, index (value)}
+		<label
+			class:bg-primaryAccentFocus={value === group}
+			class:border-r={index !== values.length - 1}
+			class="w-full bg-primaryAccent h-8 flex items-center justify-center transition-colors duration-500 cursor-pointer border-gray-200 border-opacity-20">
+			<input class="hidden" type="radio" {value} bind:group />
+			<p class="font-semibold">{value}</p>
+		</label>
+	{/each}
+</div>
