ef467098f85e730c4e3cfe3933560bcd69879e13
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2021-04-14 20:28:19 +0200

    Score component

---
+++ frontend/src/components/Lobby/Score.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import {Team} from '../../model/team';
+	import {game} from '../../stores/game';
+</script>
+
+<div class="flex items-center">
+	<div class="w-8 h-8 bg-primaryFirstTeam" />
+	<p class="text-7xl font-semibold">
+		{$game.score[Team.FIRST]}:{$game.score[Team.SECOND]}
+	</p>
+	<div class="w-8 h-8 bg-primarySecondTeam" />
+</div>
