6ad775f2798fba6dfe739fad362c1d9eaf9af59e
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2021-04-18 17:28:59 +0200

    Styling adjustment

--- frontend/src/components/Lobby/Score.svelte
+++ frontend/src/components/Lobby/Score.svelte
@@ -5,7 +5,7 @@
 
 <div class="flex items-center">
 	<div class="w-8 h-8 bg-primaryFirstTeam" />
-	<p class="text-7xl font-semibold">
+	<p class="text-7xl font-semibold mx-1">
 		{$game.score[Team.FIRST]}:{$game.score[Team.SECOND]}
 	</p>
 	<div class="w-8 h-8 bg-primarySecondTeam" />
