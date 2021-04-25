a365dccbd1d553ea2eda619df2f69a1bd4f2fc71
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2021-04-25 15:44:25 +0200

    Rebuilt gameSummary

--- frontend/src/components/Lobby/GameSummary.svelte
+++ frontend/src/components/Lobby/GameSummary.svelte
@@ -3,15 +3,49 @@
 	import Spacer from '../commons/Spacer.svelte';
 	import {game} from '../../stores/game';
 	import {ResultType} from '../../model/resultType';
-	import {teamsNames} from '../../helpers/teamNames';
+	import {get} from 'svelte/store';
+	import {players} from '../../stores/players';
+	import {socket} from '../../stores/socket';
+
+	const checkWhetherPlayerHaveWon = (): boolean => {
+		const localPlayers = get(players);
+		const localSocket = get(socket);
+		const localGameResult = get(game).result;
+
+		if (!localPlayers.length || !localSocket || !localGameResult) {
+			return false;
+		}
+		return localPlayers.find((player) => player.id === localSocket.id)?.team === localGameResult.winner;
+	};
+
+	const didPlayerWon = checkWhetherPlayerHaveWon();
 </script>
 
 <div class="flex flex-col items-center w-full">
 	<Spacer y={2}>
 		<Score />
 	</Spacer>
-
 	{#if $game.result?.type === ResultType.SCORE && $game.result.winner !== 'TIE'}
-		{teamsNames[$game.result.winner]} team won!
+		<p>{didPlayerWon ? 'Your team have won, congratulations!' : 'Opponent was better, better luck next time!'}</p>
+	{/if}
+	{#if $game.result?.type === ResultType.OUT_OF_CARDS}
+		<p>Unfortunately you have got out of cards :(</p>
+		<p>
+			{#if $game.result.winner === 'TIE'}
+				There was a tie!
+			{:else}
+				<p>{didPlayerWon ? 'Your team have won, congratulations!' : 'Opponent was better, better luck next time!'}</p>
+			{/if}
+		</p>
+	{/if}
+	{#if $game.result?.type === ResultType.LEAVE || $game.result?.type === ResultType.KICKED}
+		<p>Unfortunately too many players left or have been kicked :(</p>
+		<p>
+			{#if $game.result.winner === 'TIE'}
+				There was a tie!
+			{:else}
+				<p>{didPlayerWon ? 'Your team have won, congratulations!' : 'Opponent was better, better luck next time!'}</p>
+			{/if}
+		</p>
 	{/if}
 </div>
